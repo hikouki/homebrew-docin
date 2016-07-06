9a91ce9bee1bc83b37dd1fd2d74bbeacd0bdeb07
hikouki <ame.kh000@gmail.com>
2016-07-06 10:13:21 +0900

    Fix formula.

--- docin.rb
+++ docin.rb
@@ -3,7 +3,7 @@ require "formula"
 class Docin < Formula
   homepage "https://github.com/hikouki/docin"
   version "1.0.0"
-  dec "Go into a docker container by fzf interface."
+  desc "Go into a docker container by fzf interface."
   url "https://github.com/hikouki/docin/archive/v#{version}.tar.gz"
   sha256 "1ed13ea38198dc7e3b1399acbf97bc5e22db9df26fb2b8d4fda826b158e3e7f0"
   head "https://github.com/hikouki/docin.git"
